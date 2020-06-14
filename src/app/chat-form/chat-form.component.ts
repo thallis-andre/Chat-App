9498005629ee7f5b78b664bf9f95363231939586
thallis-andre <thallis-andre@hotmail.com>
2020-06-14 01:59:48 -0300

    Salvando mensagens no firebase

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from './../services/chat.service';
 
 @Component({
   selector: 'app-chat-form',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatFormComponent implements OnInit {
 
-  constructor() { }
+  message: string
+
+  constructor(
+    private chat: ChatService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  send(){
+    this.chat.sendMessage(this.message)
+  }
+
+  handleSubmit(event){
+    if(event.keyCode === 13){
+      this.send()
+    }
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,77 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase/app';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { ChatMessage } from '../models/chat-message.model';
+
+@Injectable()
 export class ChatService {
+  user: firebase.User;
+  chatMessages: FirebaseListObservable<ChatMessage[]>;
+  chatMessage: ChatMessage;
+  userName: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {
+    // this.afAuth.authState.subscribe(auth => {
+    //   if (auth !== undefined && auth !== null) {
+    //     this.user = auth;
+    //   }
+
+    //   this.getUser().subscribe(a => {
+    //     this.userName = a.displayName;
+    //   });
+    // });
+  }
+
+  // getUser() {
+  //   const userId = this.user.uid;
+  //   const path = `/users/${userId}`;
+  //   return this.db.object(path);
+  // }
+
+  // getUsers() {
+  //   const path = '/users';
+  //   return this.db.list(path);
+  // }
+
+  sendMessage(msg: string) {
+    const timestamp = this.getTimeStamp();
+    // const email = this.user.email;
+    const email = 'teste@hotmail.com';
+    this.chatMessages = this.getMessages();
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      // userName: this.userName,
+      userName: 'teste',
+      email: email
+    });
+
+    console.log('Enviando mensagem')
+  }
+
+  getMessages(): Observable<ChatMessage[]> {
+    return this.db.list('/messages', ref => {
+      return ref.limitToLast(25).orderByKey()
+    });
+  }
+
+  getTimeStamp() {
+    const now = new Date();
+    const date = now.getDate() +
+      (now.getMonth() + 1) + '/' +
+      now.getFullYear() + '/';
+
+    const time = now.getHours() + ':' +
+      now.getMinutes() + ':' +
+      now.getSeconds();
+
+    return (date + ' ' + time);
+  }
 }
